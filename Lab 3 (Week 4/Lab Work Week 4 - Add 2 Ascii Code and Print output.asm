bfa83f905629f96732ecb2d6a9ba65c5d06a43e8
bscs2012374 <73938648+bscs2012374@users.noreply.github.com>
2021-11-06 09:24:29 +0500

    Add files via upload

---
+++ Lab 3 (Week 4/Lab Work Week 4 - Add 2 Ascii Code and Print output.asm
@@ -0,0 +1,45 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+.model small
+.stack 100h 
+.data       
+
+.code
+main proc
+
+; add your code here
+   
+   mov ah,1
+   int 21h
+   
+   mov bl,al
+   mov ah,1
+   int 21h  
+   
+   
+   add bl,al
+   sub dl,48 
+  
+   mov dl ,bl
+   mov ah,2 
+   
+   int 21h
+        
+   mov ah,4ch
+   int 21h     
+            
+            
+   
+ 
+           
+  
+   
+endp
+end main
+   
+
+
+
+
+
