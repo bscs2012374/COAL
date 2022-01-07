66fb72ac05e225ca4a8f4419c60a2c8e348213c8
bscs2012374 <73938648+bscs2012374@users.noreply.github.com>
2022-01-08 03:49:22 +0500

    Add files via upload

---
+++ Lab 3 (Week 4/Week 4 - Subtraction of two numbers.asm
@@ -0,0 +1,23 @@
+.model large
+.stack 100h
+.data
+.code
+main proc   
+    
+    mov ah,1
+    int 21h   
+    mov dl,al 
+   
+    mov ah,1
+    int 21h  
+    sub dl,al
+   
+    add dl,48  
+   
+    mov ah,02
+    int 21h
+   
+    mov ah,4ch
+    int 21h
+    main endp
+end main
